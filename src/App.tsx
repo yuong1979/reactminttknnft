4756436d5c0d36c884735a090d998da9320df507
yuong1979 <yuong1979@gmail.com>
2022-07-07 18:31:26 +0800

    added wrapping sending sol - not working

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 // import './MintToken';
 import Minttoken from './MintToken';
 import MintNft from './MintNft';
+import SendSol from './sendSol';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <header className="App-header">
         <Minttoken />
         <MintNft />
-
+        <SendSol />
       </header>
     </div>
   );
